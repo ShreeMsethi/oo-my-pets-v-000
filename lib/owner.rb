628f4fe10b475404ea81a406221ee6a276e7cdc0
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-06-23 23:21:50 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,11 +41,11 @@ class Owner
   
   
   def buy_cat(name)
-    @@all << Cat.new(name, self)
+     Cat.new(name, self)
   end
   
   def buy_dog(name)
-   @@all << Dog.new(name, self)
+   Dog.new(name, self)
   end
   
   def walk_dogs()

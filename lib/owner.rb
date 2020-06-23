d2c15f245f13b1ed1f9d41a176fc50fe8a8777e3
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-06-23 22:58:33 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+   
+  end
+
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end  
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def cats
+    Cat.all.select {|cat| cat.owner == self }
+  end 
+  
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat)
+    @@all << Cat.new(cat)
+  end
+  
+  def buy_dog(dog)
+   @@all << Dog.new(dog)
+  end
+  
+  def walk_dogs(dog, mood)
+    Dog.all.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats(cat, mood)
+    Cat.all.each{|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    cats.
+  end
+  
+  def list_pets
+  
+  end  
 end
